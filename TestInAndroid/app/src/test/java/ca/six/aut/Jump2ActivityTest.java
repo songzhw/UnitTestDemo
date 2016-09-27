a8eead474733859adc00b1fd2ba2108393f0099f
songzhw <songzhw2012@gmail.com>
2016-09-27 18:22:32 -0400

    test the Activity with intent

---
+++ TestInAndroid/app/src/test/java/ca/six/aut/Jump2ActivityTest.java
@@ -0,0 +1,54 @@
+package ca.six.aut;
+
+import android.content.Intent;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricGradleTestRunner;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import cn.six.aut.BuildConfig;
+import cn.six.aut.R;
+import cn.six.jump.JumpToActivity;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertNull;
+
+/**
+ * Created by songzhw on 2016-09-27
+ */
+@RunWith(RobolectricTestRunner.class)  // 不用这个， 就在buildActivity时会错的
+@Config(constants = BuildConfig.class) // 修复说找不到AndroidManifest的warning
+public class Jump2ActivityTest {
+    public JumpToActivity toActivity;
+    public TextView tv;
+
+    @Before
+    public void setUp(){
+        Intent it = new Intent();
+        it.putExtra(JumpToActivity.KEY_EXTRA_MESSAGE, "20160927Test");
+
+        // Robolectric.buildActivity() will cause tv to be null
+        toActivity = Robolectric.buildActivity(JumpToActivity.class)
+                        .withIntent(it).create().get();
+        tv = (TextView) toActivity.findViewById(R.id.tv_jumpto_display);
+    }
+
+    @Test
+    public void testActvNotNull() {
+        assertNotNull(toActivity);
+    }
+
+    @Test
+    public void testTextAfterShowing(){
+        assertNotNull(tv);
+        System.out.println("szw tv.text = "+tv.getText());
+        assertEquals("20160927Test", tv.getText());
+    }
+}

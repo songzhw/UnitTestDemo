ff05ecbf6646df40da6175943988c6b77b89fc6d
songzhw <songzhw2012@gmail.com>
2016-09-29 22:45:47 -0400

    Robolectric : test lifycycle

---
+++ TestInAndroid/app/src/main/java/cn/six/robolectric/LifeCycleActivity.java
@@ -0,0 +1,26 @@
+package cn.six.robolectric;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LifeCycleActivity extends Activity {
+    public String stage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        stage = "onCreate";
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        stage = "onResume";
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        stage = "onDestroy";
+    }
+}
---
+++ TestInAndroid/app/src/test/java/ca/six/aut/robolectric/LifeCycleAndResTest.java
@@ -0,0 +1,39 @@
+package ca.six.aut.robolectric;
+
+import android.app.Application;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+import org.robolectric.util.ActivityController;
+
+import cn.six.aut.BuildConfig;
+import cn.six.aut.R;
+import cn.six.robolectric.LifeCycleActivity;
+
+import static junit.framework.Assert.assertEquals;
+
+@RunWith(RobolectricTestRunner.class)  // 不用这个， 就在buildActivity时会错的
+@Config(constants = BuildConfig.class) // 修复说找不到AndroidManifest的warning
+public class LifeCycleAndResTest {
+
+    @Test
+    public void testLifecycle(){
+        ActivityController<LifeCycleActivity> actvController =
+            Robolectric.buildActivity(LifeCycleActivity.class).create();
+        LifeCycleActivity actv = actvController.get();
+
+        actvController.start();
+        assertEquals("onCreate", actv.stage);
+
+        actvController.resume();
+        assertEquals("onResume", actv.stage);
+
+        actvController.destroy();
+        assertEquals("onDestroy", actv.stage);
+    }
+
+}

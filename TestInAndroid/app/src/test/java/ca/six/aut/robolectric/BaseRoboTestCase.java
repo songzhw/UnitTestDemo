ba828f4fe591fd14aa40c1b4ea10ddae5fe98e2f
songzhw <songzhw2012@gmail.com>
2016-10-24 22:04:28 -0400

    create a base test case class for Robolectric

---
+++ TestInAndroid/app/src/test/java/ca/six/aut/robolectric/BaseRoboTestCase.java
@@ -0,0 +1,17 @@
+package ca.six.aut.robolectric;
+
+import android.content.Context;
+import android.os.Build;
+
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
+
+import cn.six.aut.BuildConfig;
+
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.JELLY_BEAN) // application = *.class
+public class BaseRoboTestCase {
+    protected Context app = RuntimeEnvironment.application;
+}

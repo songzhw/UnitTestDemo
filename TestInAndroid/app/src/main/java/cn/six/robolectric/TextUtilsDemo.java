732371bf394042419ad00b388d5cf77fbc0a4354
songzhw <songzhw2012@gmail.com>
2016-10-25 20:15:00 -0400

    test textutils

---
+++ TestInAndroid/app/src/main/java/cn/six/robolectric/TextUtilsDemo.java
@@ -0,0 +1,13 @@
+package cn.six.robolectric;
+
+import android.text.TextUtils;
+
+/**
+ * Created by songzhw on 2016-10-25
+ */
+
+public class TextUtilsDemo {
+    public boolean isEmpty(String str){
+        return TextUtils.isEmpty(str);
+    }
+}
---
+++ TestInAndroid/app/src/test/java/cn/six/robolectric/TextUtilsDemoTest.java
@@ -0,0 +1,31 @@
+package cn.six.robolectric;
+
+import org.junit.Test;
+
+import ca.six.aut.robolectric.BaseRoboTestCase;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by songzhw on 2016-10-25
+ *
+ * 没有出现网上说的， 找不到TextUtils的问题。
+ * 看来新版本Robolectric新加入了对TextUtils的支持
+ */
+public class TextUtilsDemoTest extends BaseRoboTestCase {
+
+    @Test
+    public void isEmpty() throws Exception {
+        TextUtilsDemo obj = new TextUtilsDemo();
+        boolean actual = obj.isEmpty("");
+        assertEquals(true, actual);
+    }
+
+    @Test
+    public void isEmpty2() throws Exception {
+        TextUtilsDemo obj = new TextUtilsDemo();
+        boolean actual = obj.isEmpty("abc");
+        assertEquals(false, actual);
+    }
+
+}

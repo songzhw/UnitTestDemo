5d133a5e70d63d4fadd8f4dcea953a0dbb41e15a
songzhw <songzhw2012@gmail.com>
2016-10-26 20:26:11 -0400

    A sample of http request

---
+++ TestInAndroid/app/src/main/java/cn/six/http/HttpDemo.java
@@ -0,0 +1,75 @@
+package cn.six.http;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by songzhw on 2016-10-26
+ *
+ * https://api.github.com/users/songzhw
+ *
+ * https://publicobject.com/helloworld.txt
+ *
+ */
+
+public class HttpDemo extends Activity implements View.OnClickListener {
+    private String url1 = "https://api.github.com/users/songzhw";
+    private String url2 = "https://publicobject.com/helloworld.txt";
+
+    private HttpDemo self;
+    private Button btn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        self = this;
+
+        btn = new Button(this);
+        btn.setText("Http -> ");
+        btn.setOnClickListener(this);
+        setContentView(btn);
+    }
+
+    @Override
+    public void onClick(View v) {
+        HttpEngine http = new HttpEngine();
+        http.request(url1, new HttpCallback());
+    }
+
+    private class HttpCallback implements Callback {
+        @Override
+        public void onFailure(Call call, final IOException e) {
+            self.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    btn.setText(e.toString());
+                }
+            });
+
+        }
+
+        @Override
+        public void onResponse(Call call, final Response response) throws IOException {
+            final String str = response.body().string();
+
+            self.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    btn.setText(str);
+                }
+            });
+
+
+        }
+    }
+}
---
+++ TestInAndroid/app/src/main/java/cn/six/http/HttpEngine.java
@@ -0,0 +1,30 @@
+package cn.six.http;
+
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+
+/**
+ * Created by songzhw on 2016-10-26
+ */
+
+public class HttpEngine {
+
+    public void request(final String url, final Callback callback) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                OkHttpClient client = new OkHttpClient();
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+
+                client.newCall(request)
+                        .enqueue(callback);
+            }
+        }).start();
+
+    }
+
+}

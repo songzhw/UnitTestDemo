fe70841052d515ca179a9ed7251d1077e04a8666
songzhw <songzhw2012@gmail.com>
2017-03-31 23:56:10 -0400

    test : Mockito

--- TestInAndroid/app/src/test/java/cn/six/mockito/MockitoTest.java
+++ TestInAndroid/app/src/test/java/cn/six/mockito/MockitoTest.java
@@ -88,4 +88,19 @@ public class MockitoTest {
     }
 
 
+    @Test
+    public void unstubbed_invocations(){
+        //mock对象使用Answer来对未预设的调用返回默认期望值
+        List mock = mock(List.class,new Answer() {
+            @Override
+            public Object answer(InvocationOnMock invocation) throws Throwable {
+                return 999;
+            }
+        });
+        //下面的get(1)没有预设，通常情况下会返回NULL，但是使用了Answer改变了默认期望值
+        assertEquals(999, mock.get(1));
+        //下面的size()没有预设，通常情况下会返回0，但是使用了Answer改变了默认期望值
+        assertEquals(999,mock.size());
+    }
+
 }

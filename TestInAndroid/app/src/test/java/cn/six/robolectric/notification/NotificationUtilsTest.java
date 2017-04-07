636c5ac462b047b2bbe6f83cfd4c728db52e8cb1
songzhw <songzhw2012@gmail.com>
2017-04-06 20:16:59 -0400

    add test for notification

--- TestInAndroid/app/src/test/java/cn/six/robolectric/notification/NotificationUtilsTest.java
+++ TestInAndroid/app/src/test/java/cn/six/robolectric/notification/NotificationUtilsTest.java
@@ -2,7 +2,9 @@ package cn.six.robolectric.notification;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -40,6 +42,33 @@ public class NotificationUtilsTest extends BaseRoboTestCase {
     private Notification getNotification() {NotificationUtils utils = new NotificationUtils();
         utils.showNotification(app);
         return shadow.getAllNotifications().get(0);
+    }
+
+
+    @Test
+    public void call_action1GoesB(){
+        Notification notification = getNotification();
+        // 说是有API 19才有的方法。 但这是测试方法， 所以没事的
+        PendingIntent pendingIntent = notification.actions[0].actionIntent;
+        Intent action1Intent = shadowOf(pendingIntent).getSavedIntent();
+        String nextClasName = action1Intent.getComponent().getClassName();
+        assertEquals(nextClasName, Notification_B.class.getName());
+
+        String value = action1Intent.getStringExtra("key");
+        assertEquals(value, "This is B");
+    }
+
+
+    @Test
+    public void call_contentIntentGoesToE() {
+        Notification notification = getNotification();
+        PendingIntent pendingIntent = notification.contentIntent;
+        Intent contentIntent = shadowOf(pendingIntent).getSavedIntent();
+        String nextClasName = contentIntent.getComponent().getClassName();
+        assertEquals(nextClasName, Notification_E.class.getName());
+
+        String value = contentIntent.getStringExtra("key");
+        assertEquals(value, "This is E");
     }
 
 

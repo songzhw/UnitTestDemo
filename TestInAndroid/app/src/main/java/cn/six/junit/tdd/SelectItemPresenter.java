8ea201f0633aa367cd0151c3764d6982348cae88
songzhw <songzhw2012@gmail.com>
2017-04-22 21:46:27 -0400

    TDD 02: fail the test

--- TestInAndroid/app/src/main/java/cn/six/junit/tdd/SelectItemPresenter.java
+++ TestInAndroid/app/src/main/java/cn/six/junit/tdd/SelectItemPresenter.java
@@ -1,7 +1,13 @@
 package cn.six.junit.tdd;
 
 public class SelectItemPresenter {
+    private int[] data;
+
+    public SelectItemPresenter() {
+        data = new int[]{1,3,5,7,9,11,12,13,15,17,19,20};
+    }
+
     public int[] threeTimes() {
-        return new int[0];
+        return new int[1];
     }
 }

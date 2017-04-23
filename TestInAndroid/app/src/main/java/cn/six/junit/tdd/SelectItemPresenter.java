462453636ec08e6b23e7cf45079b96a31c308c06
songzhw <songzhw2012@gmail.com>
2017-04-22 21:48:21 -0400

    TDD 03: pass the test

--- TestInAndroid/app/src/main/java/cn/six/junit/tdd/SelectItemPresenter.java
+++ TestInAndroid/app/src/main/java/cn/six/junit/tdd/SelectItemPresenter.java
@@ -4,10 +4,18 @@ public class SelectItemPresenter {
     private int[] data;
 
     public SelectItemPresenter() {
-        data = new int[]{1,3,5,7,9,11,12,13,15,17,19,20};
+        data = new int[]{1, 3, 5, 7, 9, 11, 12, 13, 15, 17, 19, 20};
     }
 
     public int[] threeTimes() {
-        return new int[]{1};
+        int[] ret = new int[data.length];
+        int index = 0;
+        for (int i : data) {
+            if (i % 3 == 0) {
+                ret[index] = i;
+                index++;
+            }
+        }
+        return ret;
     }
 }

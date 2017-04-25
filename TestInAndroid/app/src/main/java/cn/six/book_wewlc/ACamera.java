0aa3f61dd6c10cf9edb4dc691c54d8b7b01e8f10
songzhw <songzhw2012@gmail.com>
2017-04-24 20:20:27 -0400

    book: how to test a private method

--- TestInAndroid/app/src/main/java/cn/six/book_wewlc/ACamera.java
+++ TestInAndroid/app/src/main/java/cn/six/book_wewlc/ACamera.java
@@ -3,8 +3,13 @@ package cn.six.book_wewlc;
 public class ACamera {
     public boolean isSnap = false;
     public int x = 0;
-    private void setSnapRegion(int x, int y, int dx ,int dy){
+
+    protected void setSnapRegion(int x, int y, int dx, int dy) {
         this.x = x + dx;
-    };
-    public void snap(){isSnap = true;};
+    }
+
+    public void snap() {
+        isSnap = true;
+    }
+
 }
--- TestInAndroid/app/src/test/java/cn/six/book_wewlc/ACameraTest.java
+++ TestInAndroid/app/src/test/java/cn/six/book_wewlc/ACameraTest.java
@@ -5,11 +5,20 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ACameraTest {
-
     @Test
     public void testSetSnapRegion(){
-
+        TestACamera camera = new TestACamera();
+        camera.setSnapRegion(10, 10, 2, 3);
+        assertEquals(12, camera.x);
     }
 
+}
+
+class TestACamera extends ACamera{
+    // 子类中可以把权限放大。如这里从protected放大成public了
+    @Override
+    public void setSnapRegion(int x, int y, int dx, int dy) {
+        super.setSnapRegion(x, y, dx, dy);
+    }
 }
 

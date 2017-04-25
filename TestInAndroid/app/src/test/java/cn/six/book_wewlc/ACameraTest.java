67a9ba7887a7630c883c4b1b05ab99e28df9bf75
songzhw <songzhw2012@gmail.com>
2017-04-24 20:14:45 -0400

    book: how to test a private method

---
+++ TestInAndroid/app/src/test/java/cn/six/book_wewlc/ACameraTest.java
@@ -0,0 +1,15 @@
+package cn.six.book_wewlc;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ACameraTest {
+
+    @Test
+    public void testSetSnapRegion(){
+
+    }
+
+}
+

617beb2c9f3210a2714eeb945aa156f2f257cb0d
songzhw <songzhw2012@gmail.com>
2017-05-01 18:27:17 -0400

    write unit test code

--- Test-Todo/app/src/test/java/ca/six/todo/addedittask/AddEditTaskPresenterTest.java
+++ Test-Todo/app/src/test/java/ca/six/todo/addedittask/AddEditTaskPresenterTest.java
@@ -1,16 +1,23 @@
-package ca.six.todo.addedittask;
 
+package ca.six.todo.addedittask;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import ca.six.todo.Injection;
+import ca.six.todo.data.Task;
 import ca.six.todo.data.source.TasksDataSource;
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.verify;
 
 public class AddEditTaskPresenterTest {
     @Mock AddEditTaskContract.View view;
     @Mock TasksDataSource repo;
+
+    public static final String TITLE = "title";
+    public static final String DESCRIPTION = "description";
 
     @Before
     public void setUp(){
@@ -18,36 +25,30 @@ public class AddEditTaskPresenterTest {
     }
 
 
-    // AddEditTaskPresenter presenter = new AddEditTaskPresenter(null, repo, view, true);
     @Test
-    public void start() throws Exception {
+    public void createTask_showEmptyView() throws Exception {
         AddEditTaskPresenter presenter = new AddEditTaskPresenter(null, repo, view, true);
-
+        // do nothing. so it's not testable
     }
 
     @Test
-    public void saveTask() throws Exception {
+    public void createAndSaveEmptyTask_showError() throws Exception {
+        // taskId is null, so it is creating a new task
+        AddEditTaskPresenter presenter = new AddEditTaskPresenter(null, repo, view, true);
+        presenter.saveTask("", "");
 
+        verify(view).showEmptyTaskError();
     }
 
     @Test
-    public void populateTask() throws Exception {
+    public void createAndSaveTask_returnBackWithNewTask() throws Exception {
+        // taskId is null, so it is creating a new task
+        AddEditTaskPresenter presenter = new AddEditTaskPresenter(null, repo, view, true);
+        presenter.saveTask(TITLE, DESCRIPTION);
 
-    }
-
-    @Test
-    public void onTaskLoaded() throws Exception {
-
-    }
-
-    @Test
-    public void onDataNotAvailable() throws Exception {
-
-    }
-
-    @Test
-    public void isDataMissing() throws Exception {
-
+        Task task = new Task(TITLE, DESCRIPTION);
+        verify(repo).saveTask(task);
+        verify(view).showTasksList();
     }
 
 }
@@ -59,5 +60,14 @@ Issue01. init repo need a Context!
 Issue02. test all the public methods?
 : No. start() method is an example
 
+Issue03. why mock TaskRepo, not TaskSource in the sample?
+:
+
+Issue04: create new task failed in the test:
+        Task task = new Task(TITLE, DESCRIPTION);
+        verify(repo).saveTask(task)
+: Erro Info : "Argument(s) are different". Because the ID is different!
+Then how to solve it?
+: 
 
 */

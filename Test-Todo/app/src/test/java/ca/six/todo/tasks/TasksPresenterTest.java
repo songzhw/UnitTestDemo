2e456a1203b118b59fced9df967789e9c732e4c6
songzhw <songzhw2012@gmail.com>
2017-04-30 21:19:11 -0400

    write unit test code

---
+++ Test-Todo/app/src/test/java/ca/six/todo/tasks/TasksPresenterTest.java
@@ -0,0 +1,14 @@
+package ca.six.todo.tasks;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TasksPresenterTest {
+
+    @Test
+    public void testFirstEnters_listShouldBeEmpty(){
+
+    }
+
+}

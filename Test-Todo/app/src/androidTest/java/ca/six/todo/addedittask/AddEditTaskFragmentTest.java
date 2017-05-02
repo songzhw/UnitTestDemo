78fabc4358617cdeea83344ce1932906144dea66
songzhw <songzhw2012@gmail.com>
2017-05-02 19:49:30 -0400

    write unit test code

--- Test-Todo/app/src/androidTest/java/ca/six/todo/addedittask/AddEditTaskFragmentTest.java
+++ Test-Todo/app/src/androidTest/java/ca/six/todo/addedittask/AddEditTaskFragmentTest.java
@@ -1,5 +1,6 @@
 package ca.six.todo.addedittask;
 
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.filters.LargeTest;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
@@ -14,6 +15,7 @@ import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withEffectiveVisibility;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static org.junit.Assert.*;
@@ -29,25 +31,33 @@ public class AddEditTaskFragmentTest {
     public void createTask_emptyView(){
         onView(withId(R.id.add_task_title))
                 .check(matches(withText("")));
-    }
 
-    @Test
-    public void createTask_inputAndSaveTask(){
-        // screen is empty at first
-        onView(withId(R.id.add_task_title))
-                .check(matches(withText("")));
-
-        // type text and save
-        onView(withId(R.id.add_task_title))
-                .perform(typeText("grocery"));
-        onView(withId(R.id.add_task_description))
-                .perform(typeText("tomato, onion, garlic, eggplant"));
         onView(withId(R.id.fab_edit_task_done))
                 .perform(click());
 
-        // check the result
-
+        String message = "TO DOs cannot be empty";
+        onView(withText(message))
+                .check(matches(withEffectiveVisibility(ViewMatchers.Visibility.VISIBLE)));
 
     }
+
+//    @Test
+//    public void createTask_inputAndSaveTask(){
+//        // screen is empty at first
+//        onView(withId(R.id.add_task_title))
+//                .check(matches(withText("")));
+//
+//        // type text and save
+//        onView(withId(R.id.add_task_title))
+//                .perform(typeText("grocery"));
+//        onView(withId(R.id.add_task_description))
+//                .perform(typeText("tomato, onion, garlic, eggplant"));
+//        onView(withId(R.id.fab_edit_task_done))
+//                .perform(click());
+//
+//        // check the result
+//
+//
+//    }
 
 }

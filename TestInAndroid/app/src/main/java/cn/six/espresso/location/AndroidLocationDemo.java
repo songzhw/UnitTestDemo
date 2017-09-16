7c0c2a99218d0e9edd525002bc279c2c643fd3ee
songzhw <songzhw2012@gmail.com>
2017-09-15 22:15:19 -0400

    can not get location

--- TestInAndroid/app/src/main/java/cn/six/espresso/location/AndroidLocationDemo.java
+++ TestInAndroid/app/src/main/java/cn/six/espresso/location/AndroidLocationDemo.java
@@ -1,6 +1,9 @@
 package cn.six.espresso.location;
 
 import android.app.Activity;
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
@@ -8,15 +11,20 @@ import android.widget.TextView;
 
 import cn.six.espresso.aut.R;
 
+@SuppressWarnings("MissingPermission")
 public class AndroidLocationDemo extends Activity {
     private TextView tv;
-
-    @SuppressWarnings("MissingPermission")
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tv_btn);
         tv = (TextView)findViewById(R.id.tv_simple);
+
+        LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        Location location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
+        String lastLocation = "latitude  = " + location.getLatitude() + " ; longitude = " + location.getLongitude();
+        System.out.println("szw location = " + lastLocation);
+
     }
 
     public void onClickSimpleButton(View v) {

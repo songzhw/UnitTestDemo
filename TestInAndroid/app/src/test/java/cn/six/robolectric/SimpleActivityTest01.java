22987a55db3cd2aa09f8221ae707d2544b8ff72a
songzhw <songzhw2012@gmail.com>
2017-10-05 20:19:36 -0400

    robolectric test activity (fix NPE error)

--- TestInAndroid/app/src/test/java/cn/six/robolectric/SimpleActivityTest01.java
+++ TestInAndroid/app/src/test/java/cn/six/robolectric/SimpleActivityTest01.java
@@ -14,8 +14,15 @@ public class SimpleActivityTest01 {
     @Test
     public void testActivity(){
         /*
-        This is may get error
+        // This is may get error (tv will be null)
          Activity actv = Robolectric.buildActivity().create().get();
+         TextView tv = (TextView)actv.findViewById(R.id.tv);
+         */
+
+
+        /*
+        // If you have previous error, just change it to :
+         Activity actv = Robolectric.setupActivity();
          TextView tv = (TextView)actv.findViewById(R.id.tv);
          */
     }

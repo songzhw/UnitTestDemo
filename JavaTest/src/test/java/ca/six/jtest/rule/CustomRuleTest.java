8c6dea23bd74084d77f05de62fc55d917c66e664
songzhw <songzhw2012@gmail.com>
2018-02-02 21:55:19 -0500

    custom junit rule (multiple rule)

--- JavaTest/src/test/java/ca/six/jtest/rule/CustomRuleTest.java
+++ JavaTest/src/test/java/ca/six/jtest/rule/CustomRuleTest.java
@@ -3,19 +3,35 @@ package ca.six.jtest.rule;
 import java.util.logging.Logger;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TestWatcher;
+import org.junit.runner.Description;
+import org.junit.runners.model.Statement;
 
 public class CustomRuleTest {
 
     @Rule
     public LoggerRule logger = new LoggerRule();
 
+    @Rule
+    public TestWatcher watchman = new TestWatcher() {
+        @Override
+        public Statement apply(Statement base, Description description) {
+            System.out.println("  --> watcherman : apply()");
+            return super.apply(base, description);
+        }
+
+        @Override
+        protected void succeeded(Description description) {
+            System.out.println("  --> watchman : succeeded()");
+            super.succeeded(description);
+        }
+    };
+
     @Test
     public void checkOut() {
         final Logger log = logger.getLogger();
         log.warning("Your test is showing!");
     }
-/*
-Jan 30, 2018 6:40:24 AM ca.six.ju.CustomLogRuleTest checkOut
-WARNING: Your test is showing!
-*/
+
 }
+

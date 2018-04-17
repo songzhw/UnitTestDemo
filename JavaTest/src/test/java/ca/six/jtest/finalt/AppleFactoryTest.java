deef913ae03d3acedee38626870b4e989d88aa01
songzhw <songzhw2012@gmail.com>
2018-04-17 06:01:59 -0400

    Still failed: MissingMethodInvocationException:               when() requires an argument which has to be 'a method call on a mock'.

--- JavaTest/src/test/java/ca/six/jtest/finalt/AppleFactoryTest.java
+++ JavaTest/src/test/java/ca/six/jtest/finalt/AppleFactoryTest.java
@@ -5,11 +5,12 @@ import org.junit.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-
 import static org.mockito.Mockito.RETURNS_SMART_NULLS;
 import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 public class AppleFactoryTest {
 
@@ -18,6 +19,10 @@ public class AppleFactoryTest {
         AppleAgentListener listener = new AppleAgentListener() { /* do something */ };
         List<AppleAgentListener> listeners = new ArrayList<>();
         AppleAgent agent = mock(AppleAgent.class, RETURNS_SMART_NULLS);
+
+        when(agent.listeners).thenReturn(listeners);
+        System.out.println("szw : "+agent);
+        System.out.println("szw : "+agent.listeners);
 
         AppleFactory factory = new AppleFactory(agent);
         factory.addListener(listener); // agent.listeners是空的, 这会报NPE

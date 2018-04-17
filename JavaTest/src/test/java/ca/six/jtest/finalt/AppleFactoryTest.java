09a9003b1b3a45ae9c1bf9309d65a48f6c718dfc
songzhw <songzhw2012@gmail.com>
2018-04-16 21:59:55 -0400

    test a final memeber: failed, NullPointerException. RETURNS_SMART_NULLS does not work

--- JavaTest/src/test/java/ca/six/jtest/finalt/AppleFactoryTest.java
+++ JavaTest/src/test/java/ca/six/jtest/finalt/AppleFactoryTest.java
@@ -2,17 +2,25 @@ package ca.six.jtest.finalt;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+
+import static org.mockito.Mockito.RETURNS_SMART_NULLS;
+import static org.mockito.Mockito.RETURNS_DEEP_STUBS;
 
 public class AppleFactoryTest {
 
     @Test
     public void addListener(){
         AppleAgentListener listener = new AppleAgentListener() { /* do something */ };
-        AppleAgent agent = mock(AppleAgent.class);
+        List<AppleAgentListener> listeners = new ArrayList<>();
+        AppleAgent agent = mock(AppleAgent.class, RETURNS_SMART_NULLS);
+
         AppleFactory factory = new AppleFactory(agent);
-        factory.addListener(listener);
+        factory.addListener(listener); // agent.listeners是空的, 这会报NPE
 
         verify(agent.listeners).add(listener);
     }
